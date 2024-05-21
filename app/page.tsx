81e51b7a0e08c23e7de995d9d24141a66c2555f6
lc <codinghistoryoflance@gmail.com>
2024-05-21 20:57:36 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,18 @@ export const metadata = {
 const getRooms = async (searchParams: string) => {
   const urlParams = new URLSearchParams(searchParams);
   const queryStrings = urlParams.toString();
-
-  const res = await fetch(`${process.env.API_URL}/api/rooms?${queryStrings}`, {
-    cache: "no-cache",
-  });
-  return res.json();
+  try {
+    const res = await fetch(
+      `${process.env.API_URL}/api/rooms?${queryStrings}`,
+      {
+        cache: "no-cache",
+      }
+    );
+    const date = await res.json();
+    return date;
+  } catch (err) {
+    console.log("err =>", err);
+  }
 };
 
 export default async function HomePage({
