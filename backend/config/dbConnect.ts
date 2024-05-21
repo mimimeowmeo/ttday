c617a9553af9c86b30863f7de54a67f7fa24fb5f
lc <codinghistoryoflance@gmail.com>
2024-05-21 21:24:07 +0800

    add

--- backend/config/dbConnect.ts
+++ backend/config/dbConnect.ts
@@ -12,6 +12,7 @@ const dbConnect = async () => {
   }
   if (process.env.NODE_ENV === "production") {
     DB_URI = process.env.DB_URI!;
+    console.log(DB_URI, 1234);
   }
 
   await mongoose.connect(DB_URI);
